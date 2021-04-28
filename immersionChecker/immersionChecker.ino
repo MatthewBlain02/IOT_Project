0a6f891a401787161f8186de61580b6972fb078c
MatthewBlain02 <53818917+MatthewBlain02@users.noreply.github.com>
2021-04-28 14:00:25 +0100

    Added Feature
    Added where a notification is sent to your phone when Immersion Timer is up

--- immersionChecker/immersionChecker.ino
+++ immersionChecker/immersionChecker.ino
@@ -1,9 +1,11 @@
-
+char devid[] = "v46E0554214C3E74";
+char serverName[] = "api.pushingbox.com";
+boolean DEBUG = true;
 #include <Bridge.h>
 #include <HttpClient.h>
 int LEDPin = 2;
-
 void setup() {  
+  Bridge.begin();
   Serial.begin(9600);
   pinMode(LEDPin, OUTPUT);
   pinMode(6, OUTPUT);
@@ -11,6 +13,7 @@ void setup() {
 
 
 void loop() {
+  HttpClient client;
    digitalWrite(LEDPin, HIGH); 
   
   int sensorValue = analogRead(A0);
@@ -20,14 +23,14 @@ if (sensorValue <= 111) {
 } else {
   delay(8000);//Change Value Here To Set Timer (Set @ 8 Seconds For Testing)
   Serial.println("YOU HAVE LEFT THE IMMERSION ON | Starting Buzzer");
+  String APIRequest;
+  APIRequest = String(serverName) + "/pushingbox?devid=" + String(devid);
+  client.get (APIRequest);
   for(int i = 0; i < 10; i++){
         digitalWrite(6, HIGH);
         delay(1000);
         digitalWrite(6, LOW);
         delay(1000);
-        
-        
-  
   }
   
 }
